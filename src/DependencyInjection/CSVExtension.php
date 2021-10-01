af1fa67ff9a00ffc8e9276a78e70c056d1f2fac3
Kévin Allard <kallard@dnd.fr>
2021-10-01 08:54:39 +0200

    :tada: Initial commit

---
+++ src/DependencyInjection/CSVExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dnd\Bundle\CSVBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Console\Application;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+/**
+ * Class CSVExtension
+ *
+ * @package   Dnd\Bundle\CSVBundle\DependencyInjection
+ * @author    Agence Dn'D <contact@dnd.fr>
+ * @copyright 2004-present Agence Dn'D
+ * @license   https://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @link      https://www.dnd.fr/
+ */
+class CSVExtension extends Extension
+{
+    /**
+     * Description load function
+     *
+     * @param mixed[]          $configs
+     * @param ContainerBuilder $container
+     *
+     * @return void
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        /** @var YamlFileLoader $loader */
+        $loader = new YamlFileLoader(
+            $container, new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        if (class_exists(Application::class)) {
+            $loader->load('console.yaml');
+        }
+    }
+}
